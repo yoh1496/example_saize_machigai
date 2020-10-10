b53f633122cce94857284fed517ff475b37095a0
ishiguro-yo <yoh1496@gmail.com>
2020-10-10 09:55:47 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using OpenCvSharp;
+
+namespace dotnetcore_opencvsharp {
+    class Program {
+        static void Main(string[] args) {
+            using var src = new Mat("example.jpg", ImreadModes.Grayscale);
+            using var dst = new Mat();
+
+            Cv2.Canny(src, dst, 50, 200);
+            Cv2.ImWrite("example_canny.jpg", dst);
+            Console.WriteLine("Example done!");
+        }
+    }
+}
